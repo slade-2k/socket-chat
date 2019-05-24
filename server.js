d7e03e8cfdf79b56fce5900f8de8c696f0f9672a
Ingo Hotischeck <i.hotischeck@iks-gmbh.com>
2019-05-24 14:06:32 +0200

    changed port to use heroku env

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
-   //res.send('<h1>Hello World!</h1>');
    res.sendFile(__dirname + '/index.html');
 });
 
@@ -22,6 +21,6 @@ io.on('connection', function(socket) {
    });
 });
 
-http.listen(3000, function() {
-   console.log('listening on *:3000');
+http.listen(process.env.PORT || 3000, function() {
+   console.log('listening on port: ' + process.env.PORT ? process.env.PORT : 3000);
 });
